aac059054d2836ad2dff648a25f48783fa53473e
brokestar7 <adrian.ismael98@gmail.com>
2022-05-25 08:37:04 +0200

    creando ficheros

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-document.write("Hola Mundo ");
+document.write("Hola Mundo");
+var API_URL = 'https://icanhazdadjoke.com/';
+var HTMLResponse = document.getElementById("textoChiste");
+// function generarChiste(){
+// }
--- app.ts
+++ app.ts
@@ -1,1 +1,11 @@
 document.write("Hola Mundo");
+const API_URL = 'https://icanhazdadjoke.com/' ;
+
+const HTMLResponse = document.getElementById("textoChiste");
+
+
+// function generarChiste(){
+
+
+
+// }
